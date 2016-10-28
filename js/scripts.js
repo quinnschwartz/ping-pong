df806e8c460e13d2aa697a351e8b2f295323c672
quinnschwartz <quinnschwartz@gmail.com>
2016-10-28 13:30:37 -0700

    add if else statement to the for loop function that specifies that if a number in the array is divisible by three then it should output the string ping instead of the number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,38 @@
 //BACK END
-
-
 function pingPong (countTo) {
 var countToNum = parseInt(countTo);
 var countedNumbers = [];
-for (var i = 1; i <= countToNum; i += 1) {
-  countedNumbers.push(i);
-  }
+for (var i = 1; i <= countToNum; i ++) {
+  if (i % 3 == 0) {
+    countedNumbers.push("ping");
+  } else {
+    countedNumbers.push(i);
+    }
+  };
   return countedNumbers;
-  console.log(countedNumbers);
 };
 
+// function pingPong (countTo) {
+// var countToNum = parseInt(countTo);
+// // var finalArray = [];
+// for (var i = 1; i <= countToNum; i += 1) {
+//     countedNumbers.push(i);
+//   }
+//   return countedNumbers;
+//
+// }
+
+// function finalArray (pingPong2) {
+//   // var stringArray = countedNumbers.map(String);
+//   for (var i = 1; i <= countedNumbers.length; i += 1) {
+//   var number = countedNumbers.shift();
+//     if (number % 3 === 0) {
+//       stringArray.replace([i], "ping");
+//     } else {
+//       return alert("hi");
+//     }
+//   }
+// };
 
 //FRONT END
 $(document).ready(function(){
@@ -18,7 +40,6 @@ $(document).ready(function(){
     event.preventDefault();
 
     var countTo = $("#countTo").val();
-    var countedNumbers = [];
     var result = pingPong(countTo);
     $("#output").text(result);
   });
