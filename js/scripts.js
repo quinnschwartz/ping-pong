d18cede02fb13d6796d2f2cff07f67f2f9db4edf
quinnschwartz <quinnschwartz@gmail.com>
2016-10-28 15:20:30 -0700

    add forEach loop that lists the contents of the array one after the other in an unordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -16,35 +16,16 @@ function pingPong (input) {
       return numbersArray;
 };
 
-// function pingPong (countTo) {
-// var countToNum = parseInt(countTo);
-// // var finalArray = [];
-// for (var i = 1; i <= countToNum; i += 1) {
-//     countedNumbers.push(i);
-//   }
-//   return countedNumbers;
-//
-// }
-
-// function finalArray (pingPong2) {
-//   // var stringArray = countedNumbers.map(String);
-//   for (var i = 1; i <= countedNumbers.length; i += 1) {
-//   var number = countedNumbers.shift();
-//     if (number % 3 === 0) {
-//       stringArray.replace([i], "ping");
-//     } else {
-//       return alert("hi");
-//     }
-//   }
-// };
-
 //FRONT END
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
 
     var input = $("#input").val();
-    var result = pingPong(input);
-    $("#output").text(result);
+    var results = pingPong(input);
+    // $("#output").text(result);
+    results.forEach(function(result) {
+        $("ul").append("<li>" + result + "</li>");
+    });
   });
 });
